9b7bc93fe946e2acaaa5dce69289206e2d217132
Kheshwenda Narasimban <111118489+Kheshwen@users.noreply.github.com>
2026-08-21 00:05:59 +0800

    Fix setup.bat syntax and update PyInstaller command

--- src/setup.bat
+++ src/setup.bat
@@ -1,9 +1,5 @@
-@echo off
 cd /d "%~dp0"
 echo Building the V2.0 Executable...
-
-python -m pyinstaller --noconsole --onefile scheduler.py
-
-echo.
+python -m PyInstaller --noconsole --onefile scheduler.py
 echo Build complete! Your new .exe is in the 'dist' folder.
 pause
